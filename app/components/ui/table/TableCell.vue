a5cbf6ae00815bf3f07a34fcbe287021340ac693
Robert He <hnrobert@qq.com>
2026-08-13 02:10:46 +0800

    refactor: adjust padding and text color in TableCell, TableEmpty, and TableHead components for improved consistency

--- app/components/ui/table/TableCell.vue
+++ app/components/ui/table/TableCell.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="table-cell"
     :class="
       cn(
-        'p-4 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0 *:[[role=checkbox]]:translate-y-0.5',
+        'p-3 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0 *:[[role=checkbox]]:translate-y-0.5',
         props.class,
       )
     "
